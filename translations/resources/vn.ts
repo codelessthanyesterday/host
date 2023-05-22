5a2650149111db096d2a62511f4d4edb3e108e61
Duc Nguyen <duc@yaam.vn>
2023-05-22 18:18:02 +0700

    initial commit

---
+++ translations/resources/vn.ts
@@ -0,0 +1,15 @@
+export default {
+  greeting: 'Welcome to Jambo!',
+  errors: {
+    network: 'Network Error',
+  },
+  authen: {
+    create_account: 'Create an account',
+    login: 'Sign in',
+    sign_up: 'Sign up',
+    email_address: 'Email address',
+    your_email_address: 'Email',
+    sign_for_best: 'Sign in for the best experience.',
+    invalid_email: 'Invalid email address',
+  },
+};
